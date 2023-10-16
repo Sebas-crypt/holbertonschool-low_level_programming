1db09a93f3f22791aa59ac5ea9690f7195c84c35
Sebas-crypt <usefullseba17@gmail.com>
2023-10-16 14:20:39 -0400

    print last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,17 +12,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	if (n > 5)
 {
 	printf("%d and is greater than 5 \n", n);
 }
-	if (n > 5)
+	if (n == 0)
 {
 	printf("%d and is 0 \n", n);
 }
-	if (n == 0)
+	if (n < 6)
 {
 	printf("%d and is less than 6 and not 0 \n", n);
 }
-	if (n < 6)
 	return (0);
 }
