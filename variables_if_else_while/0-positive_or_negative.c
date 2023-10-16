97f1390c40de22b4b25b2d9f4a7464b3affee2e9
Sebas-crypt <usefullseba17@gmail.com>
2023-10-16 13:26:20 -0400

    positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 {
-	printf; ("%d is postive \n", n);
+	printf("%d is postive\n", n);
 }
 	if (n == 0)
 {
-printf; ("%d is equal \n", n);
+	printf("%d is zero\n", n);
 }
 	if (n < 0)
 {
-	printf; ("%d is negative \n", n);
+	printf("%d is negative\n", n);
 }
 	return (0);
 }
