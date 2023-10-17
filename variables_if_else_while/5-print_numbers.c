ceff39dce637be16679ad9feff6e796011429f82
Sebas-crypt <usefullseba17@gmail.com>
2023-10-17 14:01:18 -0400

    printing all single digits

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -8,14 +8,9 @@ int main(void)
 {
 	int ch;
 
-	for (ch = 48; ch < 58; ch++)
+	for (ch = 0; ch < 10; ch++)
 	{
 		putchar(ch);
-		if (ch < 57)
-		{
-			putchar(44);
-			putchar(32);
-		}
 	}
 	putchar('\n');
 	return (0);
