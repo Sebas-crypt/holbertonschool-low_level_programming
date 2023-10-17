3c8fe278338232c18f0d9f601dbd1aa9f6758f44
Sebas-crypt <usefullseba17@gmail.com>
2023-10-17 15:31:47 -0400

    printing single digit numbers with commas

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers divided by ','
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+	for (x = 48; ch < 58; ch++)
+	{
+		putchar(ch);
+		if (ch < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
