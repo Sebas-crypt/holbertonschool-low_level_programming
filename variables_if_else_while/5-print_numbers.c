2a696d79b1a66504cefc9fb9141a183a0679dec0
Sebas-crypt <usefullseba17@gmail.com>
2023-10-17 13:53:20 -0400

    printing all single digits

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (x = 48; x 58; ch++)
+	{
+		putchar(ch);
+		if (ch < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
