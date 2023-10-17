991f61f1e48c0fba4759eb3028476b34ea5edb60
Sebas-crypt <usefullseba17@gmail.com>
2023-10-17 11:02:57 -0400

    printing alphabet with caps

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <main.h>
+/**
+ * main - program
+ * int ch - introduce ch
+ * for = for every ch <= print body
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch 'a'; ch <= 'z'; ch++)
+
+	for (ch 'A'; ch <= 'Z'; ch++)
+
+	{
+		putchar (ch);
+	}
+	putchar ('\n')
+	return (0);
+}
