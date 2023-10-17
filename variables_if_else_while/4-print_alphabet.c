1d4149ea112711d437c9b402e79486d66baf8efa
Sebas-crypt <usefullseba17@gmail.com>
2023-10-17 13:14:38 -0400

    excluding q and e

---
+++ variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabet except q and e
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
