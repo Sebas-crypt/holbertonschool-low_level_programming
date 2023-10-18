e71542e0899cf6a87d01bc0e9e652f999068ece2
Sebas-crypt <usefullseba17@gmail.com>
2023-10-18 11:01:06 -0400

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - alphabet
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c = 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
