2d25acfbd10dfb8f2f1d40a09e7aa402a759f0f2
Sebas-crypt <usefullseba17@gmail.com>
2023-10-18 14:04:44 -0400

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * islower - 1 if c is lower case
+ * Return: Always 0
+ */
+int _islower(void)
+{
+	int c;
+	
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
