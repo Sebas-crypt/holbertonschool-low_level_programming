c84d8b5291ea9b38ea631fc3329581bcbdfde144
Sebas-crypt <usefullseba17@gmail.com>
2023-10-18 15:01:13 -0400

    check for numbers and letters

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- * isalpha - check if upper case or lower case
- * Return: Always 0
+ * _isalpha - check if upper case or lower case
+ * @c: character to be checked
+ * Return: 0 or 1
  */
 int _isalpha(int c)
 {
 	/* A-Z 65-90 | a-z 97-122 */
 	if ((c >= 65 && c <= 90 || (c <= 122))
-			{
-			return (1);
-			}
-			else
-			{
-			return (0);
-			}
+	{
+		return (1);
+	}
+		else
+	{
+		return (0);
+	}
 }
