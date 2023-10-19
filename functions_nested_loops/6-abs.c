8193c967e877c6ede69e65e57e29cade2b1fda25
Sebas-crypt <usefullseba17@gmail.com>
2023-10-18 22:14:28 -0400

    compute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - computes absolute value of a integer
+ * @c: number to be computed
+ * Return: 0
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_val;
+		abs_val = c * -1;
+		return (abs_val)
+	}
+	return (c);
+}
