171c89cdb5288a4008caa75c3b56775c181da5f9
Sebas-crypt <usefullseba17@gmail.com>
2023-10-19 13:04:53 -0400

    check for uppercase

---
+++ more_functions_nested_lops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main,h"
+ /**
+  * _isupper - check for uppercase
+  * @c: evaluated integer
+  * Return: 1 if upper case, if not 0
+  */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
