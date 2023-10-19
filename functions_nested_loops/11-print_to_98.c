b6357300a277257c4d794573e3fb94314159e5bf
Sebas-crypt <usefullseba17@gmail.com>
2023-10-19 00:42:13 -0400

    print numbers to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,11 +8,12 @@
 void print_to_98(int n)
 {
 	int c;
+
 	if (n > 98)
 	{
-		for (c = n; c >98; c--)
+		for (c = n; c > 98; c--)
 		{
-			printf("%d, ",for  c);
+			printf("%d, ", c);
 		}
 		printf("98\n");
 	}
