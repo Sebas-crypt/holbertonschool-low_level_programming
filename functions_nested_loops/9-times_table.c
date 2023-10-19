c5c80ce9c7d104c9c6bb9081845406908dc9dae6
Sebas-crypt <usefullseba17@gmail.com>
2023-10-18 23:57:40 -0400

    print 9 times the table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 			{
 				_putchar(s + '0');
 			}
-			if (s < 10 && a != 0);
+			if (s < 10 && a != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -31,6 +31,6 @@ void times_table(void)
 				_putchar((s % 10) + '0');
 			}
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
