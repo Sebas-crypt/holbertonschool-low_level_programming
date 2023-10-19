9d9daf6ad76bcce7493c82f9411490f6e67c9ee9
Sebas-crypt <usefullseba17@gmail.com>
2023-10-19 16:43:28 -0400

    print numbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0123456789
+ * print_numbers - print 0 to 9
  * Return: void
  */
 void print_numbers(void)
 {
-	char c;
+	int c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	_putchar(c);
+		_putchar(c + '0');
 	}
 	_putchar('\n');
 }
