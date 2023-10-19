97e782858ee49931a60a1ec0874131ea2be691a5
Sebas-crypt <usefullseba17@gmail.com>
2023-10-18 22:03:29 -0400

    print the sign of a number

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,22 +2,23 @@
 /**
  * print _sign - print sign of number
  * @n: number to be checked
+ * Return: -1, 0 or 1
  */
 int print_sign(int n)
 {
-	if (n < 0)
+	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-		_putchar(45);
+		_putchar('-');
 		return (-1);
 	}
 	else
 	{
-		_putchar(48);
+		_putchar('0');
 		return (0);
 	}
 }
