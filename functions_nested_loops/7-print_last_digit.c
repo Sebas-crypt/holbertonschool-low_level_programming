d88728da417bb0b236a396273ce4f1eb9e02c758
Sebas-crypt <usefullseba17@gmail.com>
2023-10-18 22:32:29 -0400

    print last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit of a number literally
+ * @n: number to be treated
+ * Return: value of the last digit
+ */
+int print_last_digit_(int n)
+{
+	int lst;
+
+	lst = n % 10;
+
+	if (lst < 0)
+	{
+		lst = lst * -1;
+	}
+
+	_putchar(lst + '0');
+
+	return (lst);
+}
