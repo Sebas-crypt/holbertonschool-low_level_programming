5b74ea01ac4bb2dd5ce90fe5a0bc54c92e6cf67b
Sebas-crypt <usefullseba17@gmail.com>
2023-10-19 22:05:15 -0400

    print numbers 1 to 100

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,27 +10,29 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
+		if ((i % 3) == 0 && (i % 5) == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
-		else if (i % 3 == 0)
+		else if ((i % 5) == 0 && (i % 3) != 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
-		else if (i % 5 == 0)
+		else if ((i % 5) == 0 && (i % 3) != 0)
 		{
-			printf("Buzz");
+			if (i == 100)
+			{
+				printf("Buzz\n");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
 		}
 		else
 		{
-			printf("%i", i);
-		}
-		if (i < 100)
-		{
-			printf(" ");
+			printf("%d ", i);
 		}
 	}
-	printf("\n");
 	return (0);
 }
