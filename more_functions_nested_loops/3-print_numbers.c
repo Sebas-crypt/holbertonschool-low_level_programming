11b594986dc50f6a8fef7033128bb940acfcf787
Sebas-crypt <usefullseba17@gmail.com>
2023-10-19 20:23:02 -0400

    print numbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_numbers(void)
 {
-	int c;
+	int n;
 
-	for (c = '0'; c <= '9'; c++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		_putchar(c + '0');
+		_putchar(n + '0');
 	}
 	_putchar('\n');
 }
