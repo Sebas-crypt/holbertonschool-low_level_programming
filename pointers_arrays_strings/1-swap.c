1b05f8a953a3681bdb6a5292b4778c5d06ab94b8
Sebas-crypt <usefullseba17@gmail.com>
2023-10-23 10:19:25 -0400

    swap pointers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swap a to b
+ * @a: char to be swapped
+ * @b: char to swap
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	int i;
+
+	i = *a;
+	*a = *b;
+	*b = i;
