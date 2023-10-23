068b5c5a1623c079cbcbaed7e397b61681664917
Sebas-crypt <usefullseba17@gmail.com>
2023-10-23 14:01:37 -0400

    reverse string

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,15 +10,15 @@ void print_rev(char *s)
 
 	i = 0;
 
-	for (s[i] != '\0')
+	for (s[i]; != '\0')
 	{
 		i++;
 	}
 	len = i;
 
-	for (j = len - 1; j >=0; j--)
+	for (j = len - 1; j >= 0; j--)
 	{
 		_putchar(s[j]);
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
