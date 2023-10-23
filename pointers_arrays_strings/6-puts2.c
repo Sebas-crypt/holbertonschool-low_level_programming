1b71cc240c748fc31419df8a4d4a8bedb04e1bad
Sebas-crypt <usefullseba17@gmail.com>
2023-10-23 17:43:50 -0400

    print every other character

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,16 +7,17 @@ void puts2(char *str)
 {
 	int i, len;
 
-	i= 0;
+	i = 0;
 
 	while (str[i] != '\0')
 	{
 		i++;
 	}
 
-	for (len = 0; len < i; += 2)
+	for (len = 0; len < i; len += 2)
 	{
 		_putchar(str[len]);
 	}
-	_putchar('\n')
+
+	_putchar('\n');
 }
