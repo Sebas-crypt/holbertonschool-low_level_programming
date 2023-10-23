0292ded61474f9ba72922f7c843cc947f1a0c868
Sebas-crypt <usefullseba17@gmail.com>
2023-10-23 14:37:47 -0400

    reverse a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - a function that reverses the string
+ * @s - string to reverse
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	while (s[i] = '\0');
+	{
+		i++;
+	}
+
+	while (s[i] <= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	return (*s);
+}
