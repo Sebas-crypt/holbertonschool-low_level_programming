3f53d923a878ab6db316eb7d215f4cfbb5a5268d
Sebas-crypt <usefullseba17@gmail.com>
2023-10-24 11:32:24 -0400

    convert string to integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -6,7 +6,7 @@
  */
 int _atoi(char *s)
 {
-	int i, d, n , len, f, digit;
+	int i, d, n, len, f, digit;
 
 	i = 0;
 	d = 0;
@@ -27,15 +27,15 @@ int _atoi(char *s)
 			digit = s[i] - '0';
 		if (d % 2)
 			digit = -digit;
-		n = n *10 + digit;
+		n = n * 10 + digit;
 		f = 1;
-		if (s[i + 1] < '0' || s[i +1] > '9')
+		if (s[i + 1] < '0' || s[i + 1] > '9')
 			break;
 		f = 0;
 		i++;
 		}
 		if (f == 0)
 			return (0);
-
+	}
 	return (n);
 }
