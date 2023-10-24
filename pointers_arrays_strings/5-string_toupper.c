797623bf378736236a145ea98f2d8ce0a1f611f0
Sebas-crypt <usefullseba17@gmail.com>
2023-10-24 18:21:47 -0400

    all letters to upper case

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_touper - function that changes all lower case to upper case
+ * @s: string to upper
+ * Return: pointer of upper
+ */
+char *string_toupper(char *s)
+{
+	int len, i;
+
+	len = strlen(s);
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 'a' + 'A';
+		}
+	}
+	return (s);
+}
