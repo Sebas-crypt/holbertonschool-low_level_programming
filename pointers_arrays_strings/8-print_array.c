589b100523795cf663e449472d70ed474326ce16
Sebas-crypt <usefullseba17@gmail.com>
2023-10-23 23:01:32 -0400

    print arrays

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_array - print numbers divide by commas
+ * @a: array to be printed
+ * @n: numbers to print
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+	printf("\n");
+}
