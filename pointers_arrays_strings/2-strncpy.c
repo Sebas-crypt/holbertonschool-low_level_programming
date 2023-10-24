c39b879d524b84cf2079b9e444e6db5e4b0887a5
Sebas-crypt <usefullseba17@gmail.com>
2023-10-24 14:43:18 -0400

    copy a string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strncpy - function that copies a string
+ * @dest: destination of the string
+ * @src: source string
+ * @n: number of bytes
+ */
+char *strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+	i++
+	}
+	return (dest);
+}
