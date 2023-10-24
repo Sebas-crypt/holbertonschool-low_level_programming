aa4230348648dcec3639c854d8d2cf722cb378d6
Sebas-crypt <usefullseba17@gmail.com>
2023-10-23 23:08:37 -0400

    string copying

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * *_strcpy - function that copies string
+ * @dest: where string is going
+ * @src: from where the string is gonna go
+ */
+char *strcpy(char *dest, char *src)
+{
+
