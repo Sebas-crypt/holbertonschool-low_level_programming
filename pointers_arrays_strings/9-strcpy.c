e631ac7a2490731474c002cb5f69c81f3e897151
Sebas-crypt <usefullseba17@gmail.com>
2023-10-24 10:15:26 -0400

    string copy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,10 +1,13 @@
 #include "main.h"
+
 /**
  * *_strcpy - function that copies a string
  * @dest: where string is going
  * @src: from where the string is going
+ *
  * Return: 0
  */
+
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
