79d73dff2d3c2b817c0a84d48b9c8f24d301b9bb
Sebas-crypt <usefullseba17@gmail.com>
2023-10-24 10:37:16 -0400

    string copy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -7,12 +7,20 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int len, i;
 
-	while (src[i] != '\0')
+	len = 0
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
+	dest[i] = '\0';
+
 	return (dest);
 }
