e9f1b6d6636b639a6a0a2df655e97ece3c2d6927
Sebas-crypt <usefullseba17@gmail.com>
2023-10-25 14:33:36 -0400

    locate chars on a string

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _strchr - function that locates a character string
+ * *_strchr - function that locates a character string
  * @c: first occurence
  * @s: string from where to locate
- * Retrun: 0
+ * Return: 0
  */
 char *_strchr(char *s, char c)
 {
