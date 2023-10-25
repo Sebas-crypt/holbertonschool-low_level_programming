54ea3ad54cecfbce2b673656157a286012f38797
Sebas-crypt <usefullseba17@gmail.com>
2023-10-25 13:59:10 -0400

    fill memory with a byte

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memset - function to fill a memory with a constant byte
+ * @s: memory to be filled
+ * @b: char to copy
+ * @n: times to copy
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++;)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
