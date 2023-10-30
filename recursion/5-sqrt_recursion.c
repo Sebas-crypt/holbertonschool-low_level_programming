adcf47c7faf747d6d7137a4be06455d3869ff52b
Sebas-crypt <usefullseba17@gmail.com>
2023-10-30 12:01:23 -0400

    sqrt recursion

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - return 1 if input is a prime number if not 0
+ * @n: number to calculate
+ * Return: 0
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (sqrt_recursion(n, 0));
+}
