e3051b055ba17459e183409773540917b1028668
Sebas-crypt <usefullseba17@gmail.com>
2023-10-30 11:03:33 -0400

    print string in reverse

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * print_rev_recursion - prints a string in reverse
+ * _print_rev_recursion - prints a string in reverse
  * @s: string to be printed
  */
 void _print_rev_recursion(char *s)
 {
-	if (s)
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
-		_putchar(s);
+		_putchar(*s);
 	}
 }
