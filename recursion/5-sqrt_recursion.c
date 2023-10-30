48788ce992d7a10a8b897a5bd7885bff23237984
Sebas-crypt <usefullseba17@gmail.com>
2023-10-30 13:20:16 -0400

    sqrt recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -6,7 +6,28 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	return (n, 0);
+ if (n == 0 || n == 1) 
+		{
+	        return n;
+		}
+		int sqrt = _sqrt_recursion(n / 2);
+
+		if (sqrt * sqrt == n) 
+		{
+	        return sqrt;
+		}
+		if (sqrt * sqrt > n) 
+		{
+	        return _sqrt_recursion(n / 2 - 1);
+		}
+	    return _sqrt_recursion(n / 2 + 1);
+	}
+	
+	int main() {
+		int number = 16;
+		int squareRoot = _sqrt_recursion(number);
+
+		printf("The square root of %d is %d\n", number, squareRoot);
+
+	return 0;
 }
