d01ebdcca20b756557ee84b7a0d8c13788337745
Sebas-crypt <usefullseba17@gmail.com>
2023-10-31 12:14:45 -0400

    multiply 2 numbers

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -4,14 +4,59 @@
  * main - multiplies 2 numbers
  * @argc: numbers
  * @argv: multiply
- * Return: if the program receives two arguments - 0.
- * if the program does not receive two arguments - 1.
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, prod;
+int i, d, n, len, f, digit;
 
-	if (argc != 3)
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
@@ -19,8 +64,9 @@ int main(int argc, char *argv[])
 
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
-	prod = num1 * num2;
+	result = num1 * num2;
 
-	printf("%d\n", prod);
+	printf("%d\n", result);
+
 	return (0);
 }
