5ae405c41e7c1b75ac63fd04e8f421555a862062
Sebas-crypt <usefullseba17@gmail.com>
2023-10-31 12:05:56 -0400

    multiply 2 numbers

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,61 +8,18 @@
  */
 int atoi(char *s)
 {
-	int i, j, n, len, f, dgt;
+	int num1, num2, prod;
 
-	i = 0;
-	j = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	dgt = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-			if (s[i] == '-')
-				j++;
-
-			if (s[i] >= '0' && s[i] <= '9')
-			{
-				dgt = s[i] - '0';
-				if (d % 2)
-					dgt = -dgt;
-				n = n * 10 + digit;
-				f = 1;
-				if (s[i + 1] < '0' || s[i + 1] > '9')
-					break;
-				f = 0;
-			}
-		}
-		i++;
-	}
-	if (f == 0)
-		return (0);
-	return (n);
-}
-/**
- * main - multiply numbers
- * @argc: number of args
- * @argv: array of args
- * Return: 0
- */
-int main (int argc, char * argv[])
-{
-	int result, num1, num2;
-
-	if (argc < 3 || argc > 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	num1 = _atoi(argv[1]);
-	num2 - _atoi(argv[2]);
-	result = num1 * num2;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
 
-	printf("%d\n", result);
-	return(0);
+	printf("%d\n", prod);
+	return (0);
 }
