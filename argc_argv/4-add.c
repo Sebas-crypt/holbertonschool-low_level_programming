4110ad1b8bb27d5670f25d1260a3b522229497f1
Sebas-crypt <usefullseba17@gmail.com>
2023-10-31 12:45:19 -0400

    add positive numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - add positive numbers
+ * @argc: number of args passed
+ * @argv: array of pointers to args
+ * Return: (0)
+ */
+int main(int argc, char *argv[])
+{
+	int num, digit, sum = 0;
+
+	for (num = 1; num < argc; num++)
+	{
+		for (digit = 0; argv[num][digit]; digit++)
+		{
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[num]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
