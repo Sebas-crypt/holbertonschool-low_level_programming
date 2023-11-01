85b5156063b5a205a8eb014e34de8e1896f31f1e
Sebas-crypt <usefullseba17@gmail.com>
2023-11-01 11:09:01 -0400

    add positive numbers

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -8,19 +8,71 @@
  */
 int main(int argc, char *argv[])
 {
-	int num, digit, sum = 0;
+	int i, j, n, len, f, dgt;
 
-	for (num = 1; num < argc; num++)
+	i = 0;
+	j = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	dgt = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		for (digit = 0; argv[num][digit]; digit++)
+		if (s[i] == '-')
+			++j;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			dgt = s[i] - '0';
+			if (d % 2)
+				dgt = -dgt;
+			n = n * 10 + dgt;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+	return (n);
+}
+/**
+ * main - add 2 positive num
+ * @argc: num of args
+ * @argv: array of args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum, num, i, d, k;
+
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][d] != '\0'; d++)
+		{
+			if (argv[i][d] > '9' || argv[i][d] < '0')
 			{
-				printf("Error\n");
+				puts("Error");
 				return (1);
 			}
 		}
-		sum += atoi(argv[num]);
+	}
+
+	for (k = 1; k < argc; k++)
+	{
+		num = _atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
 	}
 	printf("%d\n", sum);
 	return (0);
