6475a9fb207a696b35effc20139d5d63582d0c9c
Sebas-crypt <usefullseba17@gmail.com>
2023-11-06 10:37:10 -0400

    concatenate 2 strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -20,15 +20,15 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] || s2[i]; i++)
 		len++;
 
-	concat_str = malloc (sizeof(char) * len);
+	concat_str = malloc(sizeof(char) * len);
 
 	if (concat_str == NULL)
 		return (NULL);
 
-	for (i = 0; s1 [i]; i++)
+	for (i = 0; s1[i]; i++)
 		concat_str[concat_index++] = s1[i];
 
 	for (i = 0; s2[i]; i++)
-		concat _str[concat_index++] = s2[i];
+		concat_str[concat_index++] = s2[i];
 	return (concat_str);
 }
