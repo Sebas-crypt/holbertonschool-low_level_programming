82e51391d1a763b3bb302992dde3e6c0b961b842
Sebas-crypt <usefullseba17@gmail.com>
2023-11-06 10:04:20 -0400

    return pointer

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup_ - retunr a pointer to a newly located space in memory
+ * @str: string to be copied
+ * Return - NULL
+ */
+char *_strdup(char *str)
+{
+	char dup;
+	int i, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		len++;
+
+	dup = malloc(sizeof(char) * (len + 1));
+
+	if (dup == NULL)
+		return (NULL);
+	
+	for (i = 0; str[i]; i++)
+		dup[i] = str[i];
+
+	dup[len] = '\0';
+	return (dup);
+}
