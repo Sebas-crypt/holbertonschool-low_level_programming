baad07025d8168aa2370c2657965deee681411be
Sebas-crypt <usefullseba17@gmail.com>
2023-11-06 11:28:06 -0400

    pointer to a 2 dimensional array

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,18 +1,18 @@
 #include "main.h"
-#include <stldib.h>
+#include <stdlib.h>
 /**
  * alloc_grid - return a pointer to a 2 dimensional array of integers
  * @width: width of the 2 dimensional array
  * @height: height pf the 2 dimensonal array
  * Return: NULL
  */
-char *_strdup(char *str)
+int **alloc_grid(int width, int height)
 {
-	int twod;
+	int **twod;
 	int hgt_index, wid_index;
 
 	if (width <= 0 || height <= 0)
-		return(NULL);
+		return (NULL);
 
 	twod = malloc(sizeof(int *) * height);
 
@@ -24,7 +24,7 @@ char *_strdup(char *str)
 		twod[hgt_index] = malloc(sizeof(int) * width);
 		if (twod[hgt_index] == NULL)
 		{
-			for(; hgt_index >= 0; hgt_index--)
+			for (; hgt_index >= 0; hgt_index--)
 				free(twod[hgt_index]);
 
 			free(twod);
@@ -32,10 +32,10 @@ char *_strdup(char *str)
 		}
 	}
 
-	for(hgt_index = 0; hgt_index < height; hgt_index++)
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
 	{
 		for (wid_index = 0; wid_index < width; wid_index++)
 			twod[hgt_index][wid_index] = 0;
 	}
-	return (twod)
+	return (twod);
 }
