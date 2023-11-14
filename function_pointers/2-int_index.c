8fc5bcfb09fd98e33f921e68e1fb05f6f63aa437
Sebas-crypt <usefullseba17@gmail.com>
2023-11-14 11:56:37 -0400

    integer search

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * int_intex - searches for integer
+ * @size: size of the array
+ * @cmp: pointer to the comparing function
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
