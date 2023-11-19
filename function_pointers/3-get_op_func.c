df44b2f80429a181d0b2384b147692ac1a44405e
Sebas-crypt <usefullseba17@gmail.com>
2023-11-19 00:41:38 -0400

    get op

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -3,7 +3,7 @@
 /**
  * get_op_func - Selects the correct function to perform the operation
  * @s: The operator passed as argument.
- * Return: A pointer to the function corresponding 
+ * Return: A pointer to the function corresponding
  * to the operator given as a parameter.
  */
 int (*get_op_func(char *s))(int, int)
